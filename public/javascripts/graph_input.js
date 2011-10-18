68079065aa4cf223f6cf02585fa106e94d4e3ac2
Satheesh Ramdass <satheesh.ramdass@gmail.com>
2011-10-18 18:23:54 +0530

    Changes in the graph input layout

--- public/javascripts/graph_input.js
+++ public/javascripts/graph_input.js
@@ -6,6 +6,7 @@ $("form.sections_graph #student_ids").tokenInput(function() {
 		return '/marks/section_dyn_vals.json?type=students&section_id=' + $('form.sections_graph #section_id').val(); 	
 	}, 	{
     crossDomain: false,
+    theme: "facebook"
   });	
   
 });
